1c8709cb626aae872b7a20bfe8f43abf34625c21
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2022-03-14 08:22:18 +0530

    complete

---
+++ src/main/java/My.java
@@ -0,0 +1,70 @@
+/**
+ * Mindula Dilthushan
+ * Maven Hello Package
+ * 22-03-14
+ */
+public class My {
+
+    public static void firstName(String fname){
+        System.out.println("My First Name is : "+fname+".");
+    }
+
+    public static void lastName(String lname) {
+        System.out.println("My Last Name is : "+lname+".");
+    }
+
+    public static void midName(String mname) {
+        System.out.println("My Mid Name is : "+mname+".");
+    }
+
+    public static void surName(String sname) {
+        System.out.println("My Surname is : "+sname+".");
+    }
+
+    public static void fullName(String fullname){
+        System.out.println("My Full Name is : "+fullname+".");
+    }
+
+    public static void address(String address){
+        System.out.println("My Address is : "+address+".");
+    }
+
+    public static void mobileNumber(String mnumber){
+        System.out.println("My Mobile Number is : "+mnumber+".");
+    }
+
+    public static void emailAddress(String email){
+        System.out.println("My Email Address is : "+email+".");
+    }
+
+    public static void primaryEmailAddress(String pemail){
+        System.out.println("My Primary Email Address is : "+pemail+".");
+    }
+
+    public static void secondaryEmailAddress(String semail){
+        System.out.println("My Secondary Email Address is : "+semail+".");
+    }
+
+    public static void fax(String fax){
+        System.out.println("My Fax is : "+fax+".");
+    }
+    public static void country(String country){
+        System.out.println("My Country is : "+country+".");
+    }
+
+    public static void town(String town){
+        System.out.println("My Town is : "+town+".");
+    }
+
+    public static void zipCode(String zip){
+        System.out.println("My Zip/PostCode is : "+zip+".");
+    }
+
+    public static void yearOld(String year){
+        System.out.println("I am "+year+" years old.");
+    }
+
+    public static void birthday(Integer birthday){
+        System.out.println("My Birthday is on "+birthday+".");
+    }
+}
